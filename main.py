0bf2c1520459d59ba6eb56748b508f34c916f114
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-03-19 07:29:10 +0000

    moved all data manipulation logic from main.py to management.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!C:\Users\mic\AppData\Local\Programs\Python\Python310\python.exe
 from flask import Flask, request
 import management
-from spartan import Spartan
 
 app = Flask(__name__)
 
@@ -32,50 +31,7 @@ This API should return the spartan list as one JSON object."""
 
 @app.route("/spartan/add", methods=["POST"])
 def spartan_add():
-    spartan_data = request.json
-
-    if len(spartan_data["first_name"]) > 1:
-        s_fn = spartan_data["first_name"]
-    else:
-        return "ERROR: first name should have at least 2 characters."
-
-    if len(spartan_data["last_name"]) > 1:
-        s_ln = spartan_data["last_name"]
-    else:
-        return "ERROR: last name should have at least 2 characters."
-
-    if int(spartan_data["birth_day"]) in range(1, 32):
-        s_bd = spartan_data["birth_day"]
-    else:
-        return "ERROR: Day of birth should be a number between 1 and 31."
-
-    if int(spartan_data["birth_month"]) in range(1, 13):
-        s_bm = spartan_data["birth_month"]
-    else:
-        return "ERROR: Month of birth should be a number between 1 and 12."
-
-    if int(spartan_data["birth_year"]) in range(1900, 2005):
-        s_by = spartan_data["birth_year"]
-    else:
-        return "ERROR: Year of birth should be a number between 1900 and 2004."
-
-    if len(spartan_data["course"]) > 2:
-        s_co = spartan_data["course"]
-    else:
-        return "ERROR: Course name should have at least 3 characters."
-
-    if len(spartan_data["stream"]) > 2:
-        s_st = spartan_data["stream"]
-    else:
-        return "ERROR: Stream's name should have at least 3 characters."
-
-    if management.check_id_in_db(spartan_data["sparta_id"]):
-        return "ID already in database."
-    else:
-        s_id = spartan_data["sparta_id"]
-    temp_spartan = Spartan(s_id, s_fn, s_ln, s_bd, s_bm, s_by, s_co, s_st)
-    management.add_to_db(temp_spartan)
-    management.save_db_as_json()
+    management.add_to_db()
     return "Entry saved."
 
 
@@ -88,11 +44,7 @@ def spartan_getter(spartan_id):
 @app.route("/spartan/remove", methods=["POST"])
 def remove_spartan():
     id_var = int(request.args.get("id"))
-    management.load_db_from_file()
     management.delete_from_db(id_var)
-
-    management.save_db_as_json()
-
     return f"Entry with ID: {id_var} deleted from database."
 
 
